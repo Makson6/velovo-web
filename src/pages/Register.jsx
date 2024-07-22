7e3fd7c424da278962c1606ed4bdb8af6cd5fd70
BirushaNdegeya <birushandegeya@gmail.com>
2024-07-22 13:08:15 +0200

    finish register screen

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,14 +1,9 @@
-// import { useForm } from "react-hook-form";
-// import contact from "/contact-outline.svg";
 import Input from "../components/Input";
 import Button from "../components/Button";
 import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
-// import logoImage from "/logo.png";
-// import Button from "../components/Button";
-// import Input from "../components/Input";
-// import images from '../constants';
 import logoImage from '/public/contact-outline.svg'
+import { Link } from "react-router-dom";
 
 
 export default function Register() {
@@ -39,17 +34,17 @@ export default function Register() {
 
          <div className="absolute top-0 left-0 w-full h-full bg-transparent flex items-center justify-center z-10">
             <div className="w-full md:w-[50%] opacity-55 rounded-md flex items-center justify-center">
-               <div className="flex flex-col h-[100%] rounded-lg w-[100%] text-center justify-center items-center relative  bg-gray-700 mx-3 px-8 bg-red-500 overflow-scroll">
+               <div className="flex flex-col h-[100%] min-h-[100%]  rounded-lg w-[100%] text-center justify-center items-center relative  bg-gray-700 mx-3 px-8 overflow-scroll">
                   <div className="flex justify-between w-full items-center p-1">
                      <img
                         src={logoImage}
                         alt="logo de l'application"
-                        className="flex justify-center items-center h-10 w-32 z-20 my-3"
+                        className="flex justify-center items-center h-10 w-32 z-20 mb-2"
                      />
-                     <Button handleClick={() => goToPath('/')} title={'CANCEL'} className='' />
+                     <Button handleClick={() => goToPath('/')} title={'CANCEL'} className='mb-2' />
                   </div>
 
-                  <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-5 items-start w-full">
+                  <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-3 items-start w-full py-3">
                      <Input
                         type='text'
                         placeholder="Nom"
@@ -92,6 +87,14 @@ export default function Register() {
                         label={"Confirmer mot de passe:"}
                         register={register}
                      />
+                     <div className="flex gap-4 items-center">
+                     <Button 
+                        title={'Enregister'}
+                     />
+                     <p>
+                        Avez-vous deja un compte? <Link className="hover:underline" to={'/login'}>Login</Link>
+                     </p>
+                     </div>
                   </form>
                </div>
             </div>
